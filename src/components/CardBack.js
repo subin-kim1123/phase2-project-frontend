72aa5b69f5b5e698d8e23fa81bf0477b06e4911e
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-01 14:00:16 -0400

    change

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -5,7 +5,7 @@ export default class CardBack extends Component {
     render() {
 
         
-        let { title, readyIn, servings, calories, ingredients, steps, image, blog, winePairing, source} = this.props.recipeObj
+        let { title, readyIn, servings, calories, ingredients, steps, blog, winePairing, source} = this.props.recipeObj
         return (
             <Card onClick={this.props.handleClick}>
             <Card.Header className="dish_name">{title}</Card.Header>
@@ -13,17 +13,20 @@ export default class CardBack extends Component {
             <Card.Content>
                       
             <div className="dish_ingredients">
-                <p className="dish_ingredient1">{ingredients}</p>
-                {/* <p className="dish_ingredient2">{}</p> */}
+                <p className="dish_ingredient1">Ingredients: {ingredients.join(", ")}</p>
             </div>
             <div className="dish_instructions">
+                <br></br>
                 <p className="readyIn">Ready In: {readyIn}</p>
+                <br></br>
                 <p className="servings"> Servings: {servings}</p>
             </div>
-            <p className="dish_calories">{calories}</p>
+            <p className="dish_calories">Calories: {calories}</p>
             <p className="blog">{blog}</p>
-            <p className="wine-paring">{winePairing}</p>
-            <p className="source">{source}</p>
+            <p className="instructions">Instructions: {steps.join(" ")}</p>
+            <p className="winePairing">Wine Pairing: {winePairing}</p>
+            <br></br>
+            <p className="source">Source: {source}</p>
             </Card.Content>
             <Button onClick={this.delRecipe}>Delete</Button>
             </Card>
--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -9,7 +9,7 @@ export default class CardFront extends Component {
         return (
             <Card onClick={this.props.handleClick}>
             <Card.Header className="dish_name">{title}</Card.Header>
-            <Image className="recipe_image" src={image} alt={title}/>
+            <Image className="recipe_image" src={image} alt={title} wrapped ui={false}/>
             <Button>Edit</Button>
             <Button onClick={this.delRecipe}>Delete</Button>
             </Card>
