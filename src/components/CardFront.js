7f3b71f3947596587292819787f7143b0f9a69e7
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-01 14:41:18 -0400

    remove API key

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -5,7 +5,7 @@ export default class CardFront extends Component {
 
     
     render() {
-        let { title, readyIn, servings, calories, ingredients, steps, image, blog, winePairing, source} = this.props.recipeObj
+        let { title, image } = this.props.recipeObj
         return (
             <Card onClick={this.props.handleClick}>
             <Card.Header className="dish_name">{title}</Card.Header>
