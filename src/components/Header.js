594047178a89da3c10e6028c90b27f6fb22265be
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-01 10:33:26 -0400

    add random started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Header extends React.Component{
+
+
+   
+  addRandomRecipe = (event) => {
+    const API_KEY = 'c1f88f801b7843d3b41f2f0df1cd4538'
+  fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=1`)
+  .then((r) => r.json())
+  .then((recipeObj) => console.log(recipeObj));
+}
     render(){
       return(
-      <h1>How To Cook Bart Simpson</h1>
+        <div>
+      <h1>The Joy Of Cooking Bart Simpson</h1>
+      <button onClick={this.addRandomRecipe}>Add New Random Recipe!</button>
+      </div>
       )
     }
   }
