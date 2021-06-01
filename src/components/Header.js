a702bc633596ffb9e6add367eb078de99f19fb5e
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-01 15:27:20 -0400

    add random recipe

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,20 @@ export default class Header extends React.Component{
    
   addRandomRecipe = (event) => {
     const API_KEY = 'c1f88f801b7843d3b41f2f0df1cd4538'
+    const formattedObj = 
   fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=1`)
   .then((r) => r.json())
-  .then((recipeObj) => console.log(recipeObj))
+  .then((recipeObj) => this.props.addRecipeToState({
+    title : recipeObj.recipes[0].title,
+    readyIn : recipeObj.recipes[0].readyInMinutes,
+    servings: recipeObj.recipes[0].servings,
+    calories: recipeObj.recipes[0].pricePerServing,
+    ingredients: recipeObj.recipes[0].extendedIngredients.map(obj => obj.name),
+    steps : recipeObj.recipes[0].analyzedInstructions[0].steps.map(obj => obj.step),
+    image : recipeObj.recipes[0].image,
+    blog : recipeObj.recipes[0].summary,
+    source : recipeObj.recipes[0].source
+    }))
 }
 
     render(){
@@ -24,14 +35,5 @@ export default class Header extends React.Component{
   
 
     
-
-  title : recipeObj.recipes[0].title,
-  readyIn : recipeObj.recipes[0].readyIn,
-  calories: recipeObj.recipes[0].calories,
-  // ingredients: recipeObj.recipes[0].ingredients.join(', '),
-  // steps : recipeObj.recipes[0].steps.split(', '),
-  image : recipeObj.recipes[0].image,
-  blog : recipeObj.recipes[0].summary,
-  source : recipeObj.recipes[0].source
+ 
   
-}
