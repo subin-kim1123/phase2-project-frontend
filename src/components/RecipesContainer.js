f7831f8664faeaf4dbf0bad068cc1a9374a98d42
subin-kim1123 <79874045+subin-kim1123@users.noreply.github.com>
2021-06-02 16:05:31 -0500

    Update RecipesContainer.js

--- src/components/RecipesContainer.js
+++ src/components/RecipesContainer.js
@@ -1,13 +1,14 @@
+
 import React, { Component } from 'react'
 import Recipe from './Recipe'
 import { Card } from 'semantic-ui-react'
 
 export default class RecipesContainer extends Component {
     render() {
-        console.log('hi')
-        console.log(this.props.recipes)
-        let arrOfComponents = this.props.filterRecipes.map(recipeObj=>{
-            return <Recipe key={recipeObj.id} recipeObj={recipeObj} deleteRecipe={this.props.deleteRecipe}/>
+        // console.log('hi')
+        // console.log(this.props.recipes)
+        let arrOfComponents = this.props.recipes.map(recipeObj=>{
+            return <Recipe key={recipeObj.id} recipeObj={recipeObj} deleteRecipe={this.props.deleteRecipe} editRecipe={this.props.editRecipe}/>
         })
         return (
             <Card.Group itemsPerRow={4}>
